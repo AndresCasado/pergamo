Fix mesh loading for LBS computation
(cherry picked from commit 6c472df2bfb6f97fc85336e803006ef39d07fa31)
(cherry picked from commit 6cbbcfe1fe416914131f5b107433957c73a50838)
(cherry picked from commit 045d7d3b2a6160942a1db15d4d16910fa31fd49c)

tools/posing_tools.py
@@ -159,7 +159,7 @@ class Poser:
 def load_poser():
     smpl_model = smplx.create('data/smpl/smpl_neutral.pkl')
     smpl_layer = smplx.build_layer('data/smpl/smpl_neutral.pkl')
-    template_verts = torch.from_numpy(trimesh.load_mesh('data/mean_shirt.obj').vertices).float()
+    template_verts = torch.from_numpy(trimesh.load_mesh('data/mean_shirt.obj', process=False).vertices).float()
 
     poser = Poser(smplx_model=smpl_model)
 