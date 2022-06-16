cd3e0090a7ad9136a1dbc546f0c04d53f07adb49
AndresCasado <andres.casado.94@gmail.com>
2022-06-16 16:42:44 +0200

    Updated scripts to use only PyTorch
    (cherry picked from commit a60551884e223fbad36226e40aa8bf86f1163f0a)
    (cherry picked from commit 80078c499c9064cc37019a17b0964d21ca0e2dc9)
    (cherry picked from commit 62782ed28f1041e3b88faa40974d7481bed94c7c)

--- encoder/encode_reconstructed_poses.py
+++ encoder/encode_reconstructed_poses.py
@@ -3,8 +3,10 @@ import os
 import pickle as pkl
 
 import numpy as np
-import tensorflow as tf
+import torch
 from scipy.spatial.transform import Rotation as R
+
+from encoder.pose_encoder_10D_torch import PoseEncoder10D
 
 
 def encode_sequence(dir, sequence, pose_encoder):
@@ -24,15 +26,16 @@ def encode_sequence(dir, sequence, pose_encoder):
 
         body_pose_as_angleaxis = np.concatenate([R.from_matrix(x).as_rotvec() for x in body_poses_as_matrix_smooth])
         body_pose_as_angleaxis = body_pose_as_angleaxis.reshape([1, 23 * 3])
-        body_poses_encoded = pose_encoder.predict(body_pose_as_angleaxis)
-        output_filename = filename.replace("_bp.pkl", "_enc.pkl")
-        with open(output_filename, 'wb') as f:
-            pkl.dump(body_poses_encoded, f)
+        body_pose_as_angleaxis = torch.from_numpy(body_pose_as_angleaxis).float()
+        body_poses_encoded = pose_encoder.forward(body_pose_as_angleaxis)
+        output_filename = filename.replace("_bp.pkl", "_enc.pth")
+        torch.save(body_poses_encoded, output_filename)
 
 
 def encode_sequences(dir, sequences):
-    pose_encoder_path = "pose_encoder_10D"
-    pose_encoder = tf.keras.models.load_model(pose_encoder_path, compile=False)
+    pose_encoder_path = "../pose_encoder_10D_converted.pth"
+    pose_encoder = PoseEncoder10D()
+    pose_encoder.load_state_dict(torch.load(pose_encoder_path))
 
     for sequence in sequences:
         encode_sequence(dir, sequence, pose_encoder)
