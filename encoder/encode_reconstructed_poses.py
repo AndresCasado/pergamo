43ac5acc74fd476620312eec346047656a628428
AndresCasado <andres.casado.94@gmail.com>
2022-06-17 14:52:35 +0200

    Mathematically correct interpolation
    (cherry picked from commit 9134c0c9534d80bb5b895e8993c64da2784cb61a)
    (cherry picked from commit 4bb8ed564f94f1221e234aa7a3d7e4fddb9f0e4c)
    (cherry picked from commit 5175b0ec37d8647246c88c76548a94e49f3aae30)

--- encoder/encode_reconstructed_poses.py
+++ encoder/encode_reconstructed_poses.py
@@ -2,7 +2,6 @@ import glob
 import os
 import pickle as pkl
 
-import numpy as np
 import tensorflow as tf
 from scipy.spatial.transform import Rotation as R
 
@@ -11,20 +10,20 @@ def encode_sequence(dir, sequence, pose_encoder):
     regex_pkl = ('[0-9]' * 4) + "_bp.pkl"
     regex_pkl_filenames = glob.glob(os.path.join(dir, sequence, regex_pkl))
     regex_pkl_filenames.sort()
-    body_poses_as_matrix_smooth = None
+    body_poses_as_angleaxis_smooth = None
 
     for filename in regex_pkl_filenames:
         body_poses_as_matrix = pkl.load(open(filename, "rb"))
+        body_poses_as_angleaxis = R.from_matrix(body_poses_as_matrix).as_rotvec()
 
         # This reproduces the smoothing performed during reconstruction
-        if body_poses_as_matrix_smooth is None:
-            body_poses_as_matrix_smooth = body_poses_as_matrix
+        if body_poses_as_angleaxis_smooth is None:
+            body_poses_as_angleaxis_smooth = body_poses_as_angleaxis
         else:
-            body_poses_as_matrix_smooth = body_poses_as_matrix_smooth * 0.5 + body_poses_as_matrix * 0.5
+            body_poses_as_angleaxis_smooth = (body_poses_as_angleaxis_smooth + body_poses_as_angleaxis) * 0.5
 
-        body_pose_as_angleaxis = np.concatenate([R.from_matrix(x).as_rotvec() for x in body_poses_as_matrix_smooth])
-        body_pose_as_angleaxis = body_pose_as_angleaxis.reshape([1, 23 * 3])
-        body_poses_encoded = pose_encoder.predict(body_pose_as_angleaxis)
+        bp_angle_smooth_reshaped = body_poses_as_angleaxis_smooth.reshape(1, 23 * 3)
+        body_poses_encoded = pose_encoder.predict(bp_angle_smooth_reshaped)
         output_filename = filename.replace("_bp.pkl", "_enc.pkl")
         with open(output_filename, 'wb') as f:
             pkl.dump(body_poses_encoded, f)
